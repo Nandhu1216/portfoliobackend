25caf91c3b474f2df8800bfd3c9bac42c89e5ff9
Nandhu1216 <srinandhu1216@gmail.com>
2025-04-16 14:15:26 +0530

    commit

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get('/', (req, res) => {
   res.send('Your service is live');
 });
 
-app.post('https://portfoliobackend-itwl.onrender.com/contact', async (req, res) => {
+app.post('https://portfoliobackend-itwl.onrender.com', async (req, res) => {
   try {
     const { name, email, message } = req.body;
 
